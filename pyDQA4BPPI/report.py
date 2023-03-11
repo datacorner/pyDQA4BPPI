96a4bcd8c6b6548591f0d808cc7d50797fd7789e
datacorner <benoit@datacorner.fr>
2023-03-11 13:47:16 +0100

    new

--- pyDQA4BPPI/report.py
+++ pyDQA4BPPI/report.py
@@ -1,7 +1,18 @@
 
-class Report:
-    def __init__(self, level):
-        self.level = 1
 
-    def display(self, message):
-        print (message)
+class Report:
+    def __init__(self):
+        self.m_output = False
+        self.m_fileread = False
+    def set_output(self):
+        self.m_output = True
+    def set_fileread(self, status):
+        self.m_fileread = status
+        
+    def log(self, *message):
+        if (self.m_output):
+            final_message =""
+            for msg in message:
+                final_message += msg
+            print(final_message)
+    
